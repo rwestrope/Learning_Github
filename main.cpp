5e2ea1f10baaf72fda83db0b15bc8b30c1115b4b
rwestrope <rewestrope@gmail.com>
2025-03-08 15:08:14 -0600

    attempt 1 step 3 7.22

--- main.cpp
+++ main.cpp
@@ -7,5 +7,17 @@ using namespace std;
 #include "ShoppingCart.h"
 
 int main() {
-    
+    string customerName;
+    string currentDate;
+
+    cout << "Enter customer's name:" << endl;
+    getline(cin, customerName);
+
+    cout << "Enter today's date:" << endl;
+    getline(cin, currentDate);
+
+    cout << "Customer name: " << customerName << endl;
+    cout << "Today's date: " << currentDate << endl;
+
+    ShoppingCart cart(customerName, currentDate);
 }
